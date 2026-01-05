11024bc36d581c5352f5056fc88fabdff8b69fec
aiszin <synthaction@gmail.com>
2026-01-06 00:58:21 +0200

    Delete funkyskunk directory

--- funkyskunk/script.js
+++
@@ -1,7 +0,0 @@
-const seed = Math.random().toString(16).slice(2, 10).toUpperCase();
-document.getElementById("seed").textContent = `seed: ${seed}  |  build: unfinished`;
-
-document.getElementById("glitch").addEventListener("click", () => {
-  document.body.style.filter = `hue-rotate(${Math.floor(Math.random()*360)}deg)`;
-  setTimeout(() => (document.body.style.filter = ""), 250);
-});
